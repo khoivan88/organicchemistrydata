05a84f6f46eb8ecbcd149896021158d5ac2d34ab
khoivan88 <33493502+khoivan88@users.noreply.github.com>
2020-06-09 15:33:13 -0400

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/img");
+
+  // For 404 redirecting:
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function(err, bs) {
+        bs.addMiddleware("*", (req, res) => {
+          const content_404 = fs.readFileSync('_site/404.html');
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          // Add 404 http status code in request header.
+          // res.writeHead(404, { "Content-Type": "text/html" });
+          res.writeHead(404);
+          res.end();
+        });
+      }
+    }
+  });
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      // layouts: "_includes/_layouts",
+    }
+  };
+};
