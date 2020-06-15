3e28c4f21cec8457f672bf1e761775f2c6ba2865
khoivan88 <33493502+khoivan88@users.noreply.github.com>
2020-06-15 13:03:02 -0400

    Change repo name

--- .eleventy.js
+++ .eleventy.js
@@ -31,7 +31,7 @@ module.exports = function(eleventyConfig) {
       includes: "_includes",
       // layouts: "_includes/_layouts",
     },
-    pathPrefix: "/reich_demo/",
+    pathPrefix: "/organicchemistrydata/",
     htmlTemplateEngine: "njk"
   };
 };
