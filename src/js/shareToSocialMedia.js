2f5519112937df7cf8595a567d6eb77b52a1d4ee
khoivan88 <33493502+khoivan88@users.noreply.github.com>
2020-06-25 13:48:11 -0400

    Change to share link with hash is included
    Why: earlier, if a user goes to a general page then click on a side menu
    link, and then choose to share to socialMedia using the button on the
    footer, the hash (in-page link) is not included. This is because the
    shared URL is set from the page loading and not after an in-page link is
    click.
    This commit tried to fix that. It seems to work for Twitter. However,
    Facebook and LinkedIn still seem to not work.
    This commit also open the share link in another window or tab and not use
    pop-up window (because it is usually frowned upon for poor accessibility)

--- src/js/shareToSocialMedia.js
+++ src/js/shareToSocialMedia.js
@@ -6,41 +6,49 @@ setShareLinks();
 function socialWindow(url) {
   var left = (screen.width - 570) / 2;
   var top = (screen.height - 570) / 2;
-  var params = "menubar=no,toolbar=no,status=no,width=570,height=570,top=" + top + ",left=" + left;
+  // var params = "menubar=no,toolbar=no,status=no,width=570,height=570,top=" + top + ",left=" + left;
+
   // Setting 'params' to an empty string will launch
   // content in a new tab or window rather than a pop-up.
-  // params = "";
+  params = "";
+
   window.open(url,"NewWindow",params);
+}
+
+function getShareInfo () {
+  let pageUrl = encodeURIComponent(document.URL);
+  let tweet = encodeURIComponent($("meta[property='og:description']").attr("content"));
+  // console.log(`pageUrl is ${pageUrl}`)
+  // console.log(`tweet is ${tweet}`)
+  // console.log(`typeof tweet is ${typeof tweet}`)
+  return [pageUrl, tweet]
 }
 
 function setShareLinks() {
   // console.log('Share to Social Media Button JavaScript!')
 
-  var pageUrl = encodeURIComponent(document.URL);
-  var tweet = encodeURIComponent($("meta[property='og:description']").attr("content"));
-  // console.log(`pageUrl is ${pageUrl}`)
-  // console.log(`tweet is ${tweet}`)
-  // console.log(`typeof tweet is ${typeof tweet}`)
-
-  $(".social-share.facebook").on("click", function() {
-    url = "https://www.facebook.com/sharer.php?u=" + pageUrl;
-    socialWindow(url);
+  $(".social-share.facebook").on('click', function () {
+    let [pageUrl, tweet] = getShareInfo()
+    url = 'https://www.facebook.com/sharer.php?u=' + pageUrl
+    socialWindow(url)
   });
 
-  $(".social-share.twitter").on("click", function() {
+  $(".social-share.twitter").on('click', function () {
     // console.log(`pageUrl is ${pageUrl}`)
+    let [pageUrl, tweet] = getShareInfo()
     let url = '';
     if (tweet !== 'undefined') { // 'tweet' is of type string
-      url = "https://twitter.com/intent/tweet?url=" + pageUrl + "&text=" + tweet;
+      url = 'https://twitter.com/intent/tweet?url=' + pageUrl + '&text=' + tweet;
     }
     else {
-      url = "https://twitter.com/intent/tweet?url=" + pageUrl
+      url = 'https://twitter.com/intent/tweet?url=' + pageUrl
     }
     socialWindow(url);
   });
 
-  $(".social-share.linkedin").on("click", function() {
-    url = "https://www.linkedin.com/shareArticle?mini=true&url=" + pageUrl;
+  $(".social-share.linkedin").on('click', function () {
+    let [pageUrl, tweet] = getShareInfo()
+    url = 'https://www.linkedin.com/shareArticle?mini=true&url=' + pageUrl;
     socialWindow(url);
   })
 }
