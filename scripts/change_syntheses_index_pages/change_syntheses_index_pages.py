dbb4a1d1625763bde93817c499ded8ad2c314821
khoivan88 <33493502+khoivan88@users.noreply.github.com>
2020-07-01 12:34:22 -0400

    Add total count script

--- scripts/change_syntheses_index_pages/change_syntheses_index_pages.py
+++ scripts/change_syntheses_index_pages/change_syntheses_index_pages.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+from collections import Counter
 
 
 def main(ref_file, infile, outfile):
@@ -14,31 +15,64 @@ def main(ref_file, infile, outfile):
     # print(str(ref_soup))
     # print(linklist)
 
-    # outfile_url = f'{root_url}/years.html'
-    # outfile_html = requests.get(outfile_url)
-    # outfile_soup = BeautifulSoup(outfile_html.text, 'html.parser')
-    with open(infile, 'r') as f_in, open(outfile, 'wb') as f_out:
-        outfile_soup = BeautifulSoup(f_in.read(), 'html.parser')
-        for el in outfile_soup.find_all('a', href=True):
-            if el['href'] in linklist:
-                el.string.replace_with(linklist[el['href']])
-        output = str(outfile_soup)
-        output = output.replace(":</b> ", ":</b><br/>&nbsp;&nbsp;")    # Add new line break + spaces after each section
-        output = output.replace("</a>, ", "</a><br/>&nbsp;&nbsp;")   # Add new line break + spaces after each section
-        output = output.replace("  <b>", "<b>")
-    # print(output)
-    # with open('years.html', 'wb') as f:
-        f_out.write(output.encode('utf-8'))
+    all_hrefs = [el['href'] for el in ref_soup.find_all('a', href=True)]
+    print(f'All a tags with href: {len(all_hrefs)}')
+    distinct_hrefs = {el['href'] for el in ref_soup.find_all('a', href=True)}
+    print(f'Number of unique href: {len(distinct_hrefs)}')
+
+    # Find all href that has more than 1 count
+    c = Counter(all_hrefs)
+    repeated_href = [element for element, count in c.items() if count > 1]
+    print(repeated_href)
+    print(len(repeated_href))
+    # print(len(c.most_common(34)))
+
+    # with open(infile, 'r') as f_in, open(outfile, 'wb') as f_out:
+    #     outfile_soup = BeautifulSoup(f_in.read(), 'html.parser')
+    #     for el in outfile_soup.find_all('a', href=True):
+    #         if el['href'] in linklist:
+    #             el.string.replace_with(linklist[el['href']])
+    #     output = str(outfile_soup)
+    #     output = output.replace(":</b> ", ":</b><br/>&nbsp;&nbsp;")    # Add new line break + spaces after each section
+    #     output = output.replace("</a>, ", "</a><br/>&nbsp;&nbsp;")   # Add new line break + spaces after each section
+    #     output = output.replace("  <b>", "<b>")
+    #     # print(output)
+
+    #     f_out.write(output.encode('utf-8'))
+
+
+def count_href(ref_file):
+    linklist = {}
+    with open(ref_file, 'r') as f_ref:
+        ref_soup = BeautifulSoup(f_ref.read(), 'html.parser')
+        for el in ref_soup.find_all('a', href=True):
+            linklist[el['href']] = el.text
+    # print(str(ref_soup))
+    # print(linklist)
+
+    all_hrefs = [el['href'] for el in ref_soup.find_all('a', href=True)]
+    print(f'All a tags with href: {len(all_hrefs)}')
+    distinct_hrefs = {el['href'] for el in ref_soup.find_all('a', href=True)}
+    print(f'Number of unique href: {len(distinct_hrefs)}')
+
+    # Find all href that has more than 1 count
+    c = Counter(all_hrefs)
+    repeated_href = [(element, count) for element, count in c.items() if count > 1]
+    print(repeated_href)
+    print(len(repeated_href))
 
 
 if __name__ == "__main__":
     indir = 'groupby'
     outdir = 'new_groupby'
 
-    # outfile_name = 'years'
+    ref_file = f'{indir}/names.html'
+    # count_href(ref_file)
+
     outfile_names = ['years', 'chemoselectivity', 'named_reactions', 'reactions', 'reagents', 'rings']
+    # outfile_names = ['years']
+
     for name in outfile_names:
-        ref_file = f'{indir}/names.html'
         infile = f'{indir}/{name}.html'
         outfile = f'{outdir}/{name}.html'
         main(ref_file, infile, outfile)
