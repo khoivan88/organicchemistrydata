f2ccbac69dbc4c55ed206683b33cb88539c16b71
khoivan88 <33493502+khoivan88@users.noreply.github.com>
2020-07-04 16:37:32 -0400

    Add script to convert links from text to json
    The docx from Brian Myers needs to be converted to txt file first. This
    script converts the txt file to json

---
+++ scripts/links_to_json/links_to_json.py
@@ -0,0 +1,26 @@
+import json
+import re
+
+def links_to_json():
+    with open('page.txt', 'r') as fin, open('links.json', 'w') as fout:
+        results = []
+        result = {}
+        for line in fin:
+            line = line.strip()
+            if re.search(r'\[.*?\]', line):
+                # print(line)
+                extract = re.search(r'([^\[]*?)\[([^\]]*?)\]', line)
+                result['links'].append({
+                    'name': extract.group(1).strip(),
+                    'url': extract.group(2)
+                })
+            else:
+                results.append(result)
+                result = {'title': line, 'links': []}
+
+        results.append(result)
+        fout.write(json.dumps(results, indent=4))
+
+
+if __name__ == "__main__":
+    links_to_json()
