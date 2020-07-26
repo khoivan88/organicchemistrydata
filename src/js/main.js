3cd82aab80da5ab8b211bb413d7192935170fe63
khoivan88 <33493502+khoivan88@users.noreply.github.com>
2020-07-26 16:11:32 -0400

    Add JS function to make function wait untill element exists

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
 window.topFunction = topFunction
+window.elementReady = elementReady
+
 var mybutton = document.getElementById('toTopButton')
 
 function scrollFunction () {
@@ -38,6 +40,37 @@ window.onscroll = function () { scrollFunction() }
 //   addthisDataDiv.dataset.url = `${host}${url}`
 //   // console.log(url);
 // }
+
+
+/**
+ * Waits for an element satisfying selector to exist, then resolves promise with the element.
+ * Useful for resolving race conditions.
+ *
+ * @param selector
+ * @returns {Promise}
+ *
+ * MIT Licensed
+ * Author: jwilson8767
+ * Ref: https://stackoverflow.com/a/61747276/6596203
+ */
+function elementReady (selector) {
+  return new Promise((resolve, reject) => {
+    const el = document.querySelector(selector);
+    if (el) {resolve(el);}
+    new MutationObserver((mutationRecords, observer) => {
+      // Query for elements matching the specified selector
+      Array.from(document.querySelectorAll(selector)).forEach((element) => {
+        resolve(element);
+        //Once we have resolved we don't need the observer anymore.
+        observer.disconnect();
+      });
+    })
+      .observe(document.documentElement, {
+        childList: true,
+        subtree: true
+      });
+  });
+}
 
 document.addEventListener('DOMContentLoaded', function () {
   // console.log('main.js working!')
