aafe723bfb3f2fba47c1a1642c94c8ed1664d3e4
khoivan88 <33493502+khoivan88@users.noreply.github.com>
2020-07-28 11:57:55 -0400

    Add multilevel menu for 'Reich Collection' Why: there are a lot of items and to be more organized, they could be put into submenu. This commit implements 'Fundamentals' having sublevel items. This sublevel menu will be openen upon clicked.

--- src/js/main.js
+++ src/js/main.js
@@ -41,7 +41,6 @@ window.onscroll = function () { scrollFunction() }
 //   // console.log(url);
 // }
 
-
 /**
  * Waits for an element satisfying selector to exist, then resolves promise with the element.
  * Useful for resolving race conditions.
@@ -55,22 +54,40 @@ window.onscroll = function () { scrollFunction() }
  */
 function elementReady (selector) {
   return new Promise((resolve, reject) => {
-    const el = document.querySelector(selector);
-    if (el) {resolve(el);}
+    const el = document.querySelector(selector)
+    if (el) { resolve(el) }
     new MutationObserver((mutationRecords, observer) => {
       // Query for elements matching the specified selector
       Array.from(document.querySelectorAll(selector)).forEach((element) => {
-        resolve(element);
-        //Once we have resolved we don't need the observer anymore.
-        observer.disconnect();
-      });
+        resolve(element)
+        // Once we have resolved we don't need the observer anymore.
+        observer.disconnect()
+      })
     })
       .observe(document.documentElement, {
         childList: true,
         subtree: true
-      });
-  });
+      })
+  })
 }
+
+// ------------------------------------------------------- //
+// Multi Level dropdowns
+// ------------------------------------------------------ //
+// $("ul.dropdown-menu [data-toggle='dropdown']").on('click', function (event) {
+$("ul.dropdown-menu [data-toggle='dropdown']").on('click', function (event) {
+  event.preventDefault()
+  event.stopPropagation()
+
+  $(this).siblings().toggleClass('show')
+
+  if (!$(this).next().hasClass('show')) {
+    $(this).parents('.dropdown-menu').first().find('.show').removeClass('show')
+  }
+  $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function (e) {
+    $('.dropdown-submenu .show').removeClass('show')
+  })
+})
 
 document.addEventListener('DOMContentLoaded', function () {
   // console.log('main.js working!')
