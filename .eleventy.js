e95be7e324af4d34baaf42240706fa00fad8e0e3
khoivan88 <33493502+khoivan88@users.noreply.github.com>
2020-08-13 15:53:59 -0400

    Remove extra favicon icons and rel tag. Just use the traditional favicon

--- .eleventy.js
+++ .eleventy.js
@@ -7,7 +7,7 @@ const eleventyInputDir = 'src';
 const eleventyOutputDir = '_site';
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.setTemplateFormats(["html", "liquid", "njk", "ejs", "md", "hbs", "mustache", "haml", "pug", "11ty.js", "pdf", "gif", "png", "ico", "webmanifest"]);
+  eleventyConfig.setTemplateFormats(["html", "liquid", "njk", "ejs", "md", "hbs", "mustache", "haml", "pug", "11ty.js", "pdf", "gif", "ico"]);
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/img");
