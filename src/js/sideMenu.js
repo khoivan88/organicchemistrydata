b45a3256e55c32bed447904148de459935bab3a0
khoivan88 <33493502+khoivan88@users.noreply.github.com>
2020-08-24 16:21:00 -0400

    Redo footer to remove all blackboard-related content Also simplify DOM content by removing unnecessary elements

--- src/js/sideMenu.js
+++ src/js/sideMenu.js
@@ -39,7 +39,7 @@ function closeNavOnSmallScreen () {
  * Put footer section at the end of pages with toggle side menu
  */
 function adjustFooterPosition () {
-  let footer = document.querySelector('footer#bb-footer')
+  let footer = document.querySelector('footer#docdata-footer')
   let fullList = document.querySelector('.full-list')
   let references = document.querySelector('#references')
   if (fullList || references) {
