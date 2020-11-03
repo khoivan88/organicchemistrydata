2c0d079bc6a3350d8ad35ae965d22495591125d7
Khoi Van <33493502+khoivan88@users.noreply.github.com>
2020-11-03 13:53:41 -0500

    Change: separate 'partials' and complete 'layouts' into their own folders in 'src/_includes'

--- .eleventy.js
+++ .eleventy.js
@@ -94,8 +94,7 @@ module.exports = function (eleventyConfig) {
 
   return {
     dir: {
-      input: "src",
-      includes: "_includes",
+      input: "src"
     },
     // pathPrefix: "/organicchemistrydata",
     htmlTemplateEngine: "njk"
